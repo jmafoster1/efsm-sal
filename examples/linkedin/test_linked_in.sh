e62552b0e2d19cd8ea2d3ff64c2babd2c53b2242
Michael <jmafoster1@gmail.com>
2021-07-26 07:41:47 +0100

    New SAL files

--- examples/linkedin/test_linked_in.sh
+++ examples/linkedin/test_linked_in.sh
@@ -1,7 +1,7 @@
 source "../../assert.sh/assert.sh"
 
-OUTPUT=$(sal-smc --assertion='linked_in{2}!LTL_neverDetailed')
+OUTPUT=$(sal-smc --assertion='linked_in{0, 2}!LTL_neverDetailed')
 assert_contain "$OUTPUT" "Counterexample" "LTL_neverDetailed should be true."
 
-OUTPUT=$(sal-smc --assertion='linked_in_fixed{2}!LTL_neverDetailed')
+OUTPUT=$(sal-smc --assertion='linked_in_fixed{0, 2}!LTL_neverDetailed')
 assert_eq "$OUTPUT" "proved." "alw_must_stop_to_open should be true."
