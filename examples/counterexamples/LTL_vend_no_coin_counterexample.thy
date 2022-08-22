6b5124cf202aefa23ff92b4941422f7408551950
Michael <jmafoster1@gmail.com>
2022-08-22 11:35:31 +0100

    new proof for drinks machine LTL

--- examples/counterexamples/LTL_vend_no_coin_counterexample.thy
+++ examples/counterexamples/LTL_vend_no_coin_counterexample.thy
@@ -17,4 +17,20 @@ lemma LTL_vend_no_coin_counterexample :
   apply (simp add: possible_steps_init init_def apply_updates_def possible_steps_vend_sufficient possible_steps_2)
   by (simp add: ev_Stream implode_coin implode_vend suntil_Stream)
 
+lemma "valid_trace drinks (Some 0) <> (watch drinks (
+      (STR ''init'', [])##
+      (STR ''vend'', [])##
+      (STR ''vend'', [Str ''tea''])##
+      (STR ''vend'', [Str ''tea'', Num 88])##
+   trace))"
+  apply (simp add: make_full_observation_step)
+  apply (simp add: possible_steps_init init_def apply_updates_def possible_steps_vend_sufficient possible_steps_2)
+  apply (simp add: transitions)
+  apply (rule valid_trace.step_some, simp add: possible_steps_init transitions apply_updates_def)
+  apply (rule valid_trace.step_some, simp add: possible_steps_vend_sufficient transitions)
+  apply (rule valid_trace.step_none, simp add: possible_steps_2)
+   apply simp
+  by (metis make_full_observation_none valid_trace_None)
+
+
 end
--- examples/drinks/Drinks_Machine_LTL.thy
+++ examples/drinks/Drinks_Machine_LTL.thy
@@ -324,4 +324,157 @@ lemma LTL_output_vend_unfolded:
   apply (insert LTL_output_vend[of d t])
   by (simp add: implode_vend)
 
+lemma possible_steps_invalid_state: "s \<ge> 2 \<Longrightarrow> possible_steps drinks s r l i = {||}"
+  by (simp add: possible_steps_def drinks_def ffilter_finsert)
+
+lemma all_possible_steps_drinks:
+  "(x |\<in>| possible_steps drinks s r l i) \<Longrightarrow>
+   (x \<in> {(1, select), (1, coin), (1, vend_fail), (2, vend)})"
+  apply (simp add: possible_steps_def drinks_def)
+  by auto
+
+lemma valid_trace_simps: "valid_trace e a2 r a4 =
+(
+  (\<exists>s l i p t.
+     (a4 = \<lparr>statename = Some s, datastate = r, action = (l, i), output = p\<rparr> ## t \<and>
+     a2 = Some s \<and>
+     ((\<exists>(s', tr)|\<in>|possible_steps e s r l i.
+         evaluate_outputs tr i r = output (shd t) \<and>
+         Some s' = statename (shd t) \<and>
+         evaluate_updates tr i r = datastate (shd t) \<and> valid_trace e (Some s') (evaluate_updates tr i r) t) \<or>
+     possible_steps e s r l i = {||} \<and> [] = output (shd t) \<and> valid_trace e None r t) \<or>
+     a4 = t \<and>
+     a2 = None \<and>
+     smap (\<lambda>x. (statename x, datastate x, output x)) t = sconst (None, r, [])
+  )
+  )
+)"
+  apply (simp add: valid_trace.simps[of e a2 r a4])
+  apply standard
+   apply blast
+  by blast (* ~10s *)
+
+lemma in_possible_steps_0: "(s', tr) |\<in>| possible_steps drinks 0 r l i \<Longrightarrow> ((s', tr) = (1, select))"
+  apply (simp add: possible_steps_def drinks_def)
+  by auto
+
+lemma ex_possible_steps_0: "(\<exists>a b. (a, b) \<in> fset (possible_steps drinks 0 r l i)) = (possible_steps drinks 0 r l i = {|(1, select)|})"
+  apply (simp add: possible_steps_def drinks_def ffilter_finsert select_def)
+  by auto
+
+lemma ex_possible_steps_1: "(\<exists>a b. (a, b) \<in> fset (possible_steps drinks 1 r l i)) = (
+  possible_steps drinks 1 r l i = {|(1, coin)|} \<or>
+  possible_steps drinks 1 r l i = {|(1, vend_fail)|} \<or>
+  possible_steps drinks 1 r l i = {|(2, vend)|}
+)"
+  apply (case_tac "l=STR ''coin'' \<and> length i = 1")
+   apply (simp add: possible_steps_1_coin)
+  apply (case_tac "l=STR ''vend'' \<and> i = []")
+   apply (case_tac "r $r 2")
+    apply (simp add: drinks_vend_invalid fempty_not_finsert)
+  subgoal for a
+    apply (cases a)
+    subgoal for n
+      apply (case_tac "n \<ge> 100")
+       apply (simp add: drinks_vend_sufficient)
+      using drinks_vend_insufficient by force
+    using drinks_vend_invalid apply auto[1]
+    using drinks_vend_invalid by auto[1]
+  using drinks_no_possible_steps_1 by auto
+
+lemma coin_label: "possible_steps drinks 1 r l i = {|(1, coin)|} \<Longrightarrow> l = STR ''coin''"
+  apply (simp add: possible_steps_def drinks_def ffilter_finsert transitions)
+  apply (case_tac "STR ''coin'' = l \<and> length i = 1")
+   apply simp
+  apply (simp add: ffilter_finsert)
+  apply (case_tac "STR ''vend'' = l \<and> i = [] \<and> value_gt (Some (value.Int 100)) (r $r 2) = trilean.true")
+   apply simp
+  apply (simp add: ffilter_finsert)
+  apply (case_tac "STR ''vend'' = l \<and> i = [] \<and> \<not>? value_gt (Some (value.Int 100)) (r $r 2) = trilean.true")
+  by auto
+
+lemma vend_fail_label: "possible_steps drinks 1 r l i = {|(1, vend_fail)|} \<Longrightarrow> l = STR ''vend''"
+  apply (simp add: possible_steps_def drinks_def ffilter_finsert transitions)
+  apply (case_tac "STR ''coin'' = l \<and> length i = 1")
+   apply simp
+  apply (simp add: ffilter_finsert)
+  apply (case_tac "STR ''vend'' = l \<and> i = [] \<and> value_gt (Some (value.Int 100)) (r $r 2) = trilean.true")
+   apply simp
+  apply (simp add: ffilter_finsert)
+  apply (case_tac "STR ''vend'' = l \<and> i = [] \<and> \<not>? value_gt (Some (value.Int 100)) (r $r 2) = trilean.true")
+  by auto
+
+lemma value_gt_true_int: "(value_gt (Some (value.Int n)) (r $r m) = trilean.true) = (\<exists>n'. r $r m = Some (value.Int n') \<and> n' < n)"
+  apply (simp add: value_gt_def)
+  apply (cases "r $r m")
+   apply simp
+  subgoal for a
+    apply (cases a)
+    by auto
+  done
+
+lemma possible_steps_1_invalid: "\<not> (STR ''coin'' = l \<and> length i = 1) \<Longrightarrow>
+       \<not> (STR ''vend'' = l \<and> i = [] \<and> value_gt (Some (value.Int 100)) (r $r 2) = trilean.true) \<Longrightarrow>
+       \<not> (STR ''vend'' = l \<and> i = [] \<and> \<not>? value_gt (Some (value.Int 100)) (r $r 2) = trilean.true) \<Longrightarrow>
+      possible_steps drinks 1 r l i = {||}"
+  by (simp add: possible_steps_def drinks_def ffilter_finsert transitions)
+
+lemma smap_sconst_alw: "smap f s = sconst c \<Longrightarrow> alw (\<lambda>x. f (shd x) = c) s"
+  by (simp add: alw_iff_sdrop smap_alt)
+
+lemma smap_output: "smap (\<lambda>x. (statename x, datastate x, output x)) t = sconst (None, r, []) \<Longrightarrow> alw (output_eq []) t"
+  apply (insert smap_sconst_alw[of "(\<lambda>x. (statename x, datastate x, output x))" t "(None, r, [])"])
+  by (simp add: alw_mono)
+
+lemma alw_nxt_alw: "alw f s \<Longrightarrow> alw (nxt f) s"
+  using nxt_alw by auto
+
+lemma "ltl_apply (alw (\<lambda>xs. (label (shd xs) = STR ''vend'' \<and>
+             nxt (\<lambda>s. output (shd s) = [Some d]) xs) \<longrightarrow>
+              \<not>? value_gt (Some (value.Int 100)) (datastate (shd xs) $r 2) = trilean.true)) drinks"
+  apply (rule ltl_apply_all_states_registers)
+  subgoal for s r t
+    apply (coinduction arbitrary: s r t)
+    apply simp
+    apply (case_tac "s=Some 0")
+     apply clarsimp
+    subgoal for r t
+      apply (rule valid_trace.cases)
+         apply simp
+        apply (simp add: fBex_def Bex_def)
+        apply clarsimp
+        apply (case_tac "possible_steps drinks 0 r l i = {|(1, select)|}")
+      using drinks_0_rejects apply auto[1]
+      using ex_possible_steps_0 apply blast
+      by auto
+    apply (case_tac "s=Some 1")
+     apply clarsimp
+    subgoal for r t
+      apply (rule valid_trace.cases)
+         apply simp
+        apply (case_tac "STR ''coin'' = l \<and> length i = 1")
+         apply auto[1]
+        apply (case_tac "STR ''vend'' = l \<and> i = [] \<and> value_gt (Some (value.Int 100)) (r $r 2) = trilean.true")
+         apply (simp add: value_gt_true_int)
+         apply (erule exE)
+         apply (simp add: drinks_vend_insufficient vend_fail_def)
+         apply auto[1]
+        apply (case_tac "STR ''vend'' = l \<and> i = [] \<and> \<not>? value_gt (Some (value.Int 100)) (r $r 2) = trilean.true")
+         apply auto[1]
+        apply (simp add: possible_steps_1_invalid)
+       apply auto[1]
+      by simp
+      apply (rule valid_trace.cases)
+       apply simp
+      apply (simp add:possible_steps_invalid_state)
+     apply auto[1]
+    apply simp
+    apply standard
+    using smap_output apply fastforce
+    apply (rule disjI2)
+    apply (rule alw_mono[of "nxt (output_eq [])"])
+    using smap_output alw_nxt_alw apply blast
+    by simp
+  done
+
 end
